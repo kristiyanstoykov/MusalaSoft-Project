a14f35ba794f10478069225d413a8155ba2f0f90
justzaro <zarocuxx@yahoo.com>
2023-07-17 23:50:50 +0300

    add update and delete functions

--- ToDo/to-do-list-backend/src/main/java/com/example/todolistbackend/service/UserService.java
+++ ToDo/to-do-list-backend/src/main/java/com/example/todolistbackend/service/UserService.java
@@ -5,6 +5,7 @@ import com.example.todolistbackend.dto.UserDtoResponse;
 import com.example.todolistbackend.model.User;
 import com.example.todolistbackend.repository.UserRepository;
 import lombok.AllArgsConstructor;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.modelmapper.ModelMapper;
 import org.springframework.ui.Model;
@@ -31,13 +32,36 @@ public class UserService {
         return modelMapper.map(user, UserDtoResponse.class);
     }
 
+    public UserDtoResponse updateUser(String username, UserDto userDto) {
+        User userToUpdate = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalStateException("no such user"));
+
+        if (userRepository.findByUsername(userDto.getUsername()).isPresent()) {
+            throw new IllegalStateException("this username is already used by another user");
+        }
+
+        if (userRepository.findByEmail(userDto.getEmail()).isPresent()) {
+            throw new IllegalStateException("this email is already used by another user");
+        }
+
+        modelMapper.map(userDto, userToUpdate);
+        return modelMapper.map(userRepository.save(userToUpdate), UserDtoResponse.class);
+    }
+
+    public void deleteUser(String username) {
+        User userToDelete = userRepository.findByUsername(username)
+                .orElseThrow(() -> new IllegalStateException("no such user"));
+
+        userRepository.delete(userToDelete);
+    }
+
     public UserDtoResponse registerUser(UserDto userDto) {
 
         if (userRepository.findByEmail(userDto.getEmail()).isPresent()) {
             throw new IllegalStateException("email taken");
         }
 
-        if (userRepository.findByEmail(userDto.getUsername()).isPresent()) {
+        if (userRepository.findByUsername(userDto.getUsername()).isPresent()) {
             throw new IllegalStateException("username taken");
         }
 
