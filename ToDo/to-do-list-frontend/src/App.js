bd5957427a9a48ccbe0828f42618d0e8b51b154e
Stanislav <stanislav2177@gmail.com>
2023-07-20 09:22:52 +0300

    Todo and Account pages started but not finished

--- ToDo/to-do-list-frontend/src/App.js
+++ ToDo/to-do-list-frontend/src/App.js
@@ -1,25 +1,15 @@
-import logo from './logo.svg';
+import { BrowserRouter } from 'react-router-dom';
 import './App.css';
-import Welcome from './components/Welcome';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import About from './pages/About';
-import Home from './pages/Home';
-import Contact from './pages/Contact';
-import Login from './pages/Login';
-import SignUp from './pages/SignUp';
+import { AuthWrapper } from './auth/AuthWrapper';
+
 
 function App() {
   return (
-    <Routes>
-      
-      <Route path='/' element = {<Home/>}/>
-      <Route path='/about' element = {<About/>}/>
-      <Route path='/contact' element = {<Contact/>}/>
-     
-      <Route path='/login' element = {<Login/>}/>
-      <Route path='/signup' element = {<SignUp/>}/>
-
-    </Routes>
+    <div className="App">
+      <BrowserRouter>
+        <AuthWrapper />
+      </BrowserRouter>      
+    </div>
   );
 }
 
