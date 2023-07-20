3776d8781e8c07546ca258d31bfd992db29b3cf3
kristian <krisistoikov@gmail.com>
2023-07-20 18:32:37 +0300

    Updated account page

--- ToDo/to-do-list-frontend/src/components/pages/Account.js
+++ ToDo/to-do-list-frontend/src/components/pages/Account.js
@@ -46,21 +46,43 @@ export const Account = () => {
     <div className="page">
       
       {userData && (
-        <div className="userCard">
-          <h1>Your Account</h1>
-           
-          <h2>First Name: </h2>
-          <h3>{userData.firstName}</h3>
-
-          <h2>Last Name: </h2>
-          <h3>{userData.lastName}</h3>
-
-          <h2>Username: </h2>
-          <h3>{userData.username}</h3>
-
-          <h2>E-mail: </h2>
-          <h3>{userData.email}</h3>
+        <div className="container">
+        <div className="row">
+          <div className="col-lg-5 m-auto">
+            <div className="card mt-5">
+              <div className="card-header text-center">
+                <h4>Account information</h4>
+              </div>
+              <div className="card-body">
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label fw-bold">First Name</label>
+                  <div className="col-sm-9">
+                    <input type="text" readOnly className="form-control-plaintext" value={userData.firstName} />
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label fw-bold">Last Name</label>
+                  <div className="col-sm-9">
+                    <input type="text" readOnly className="form-control-plaintext" value={userData.lastName} />
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label fw-bold">Username</label>
+                  <div className="col-sm-9">
+                    <input type="text" readOnly className="form-control-plaintext" value={userData.username} />
+                  </div>
+                </div>
+                <div className="form-group row">
+                  <label className="col-sm-3 col-form-label fw-bold">Email</label>
+                  <div className="col-sm-9">
+                    <input type="text" readOnly className="form-control-plaintext" value={userData.email} />
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
+      </div>
         
       )}
       {error && <p>Error: {error}</p>}
