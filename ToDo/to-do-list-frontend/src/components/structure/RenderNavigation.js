6b98b2b5f94c9480ea90a3e03bad02e5b3faef1b
kristian <krisistoikov@gmail.com>
2023-07-20 11:09:52 +0300

    Finished menu styling

--- ToDo/to-do-list-frontend/src/components/structure/RenderNavigation.js
+++ ToDo/to-do-list-frontend/src/components/structure/RenderNavigation.js
@@ -1,6 +1,9 @@
+import React, { useState } from 'react';
 import { Link, Route, Routes, useNavigate } from "react-router-dom";
 import { AuthData } from "../../auth/AuthWrapper";
 import { nav } from "./navigation";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.min.js';
 
 export const RenderRoutes = () => {
   const { user } = AuthData();
@@ -24,52 +27,62 @@ export const RenderRoutes = () => {
 };
 
 export const RenderMenu = () => {
+  const [isOpen, setIsOpen] = useState(false);
+  const toggle = () => setIsOpen(!isOpen);
+  const close = () => setIsOpen(false);
   const { user, logout } = AuthData();
   const navigate = useNavigate();
 
   const MenuItem = ({ r }) => {
     return (
-      <div className="menuItem">
-        <Link to={r.path}>{r.name}</Link>
-      </div>
+        <Link 
+          className="nav-link"
+          to={r.path}>{r.name}
+        </Link>
     );
   };
 
   return (
-    <div className="menu">
-      {nav.map((r, i) => {
-        if (!r.isPrivate && r.isMenu) {
-          return <MenuItem key={i} r={r} />;
-        } else if (user.isAuthenticated && r.isMenu) {
-          return <MenuItem key={i} r={r} />;
-        } else return false;
-      })}
-
-      {user.isAuthenticated ? (
-        <div className="menuItem">
-          <Link to={"#"} onClick={logout}>
-            Log out
-          </Link>
+    <nav className="navbar navbar-expand-lg bg-body-tertiary">
+      <div className="container-fluid">
+        <a className="navbar-brand" href="#">Navbar</a>
+        <button className={`navbar-toggler ${isOpen ? '' : 'collapsed'}`} type="button" onClick={toggle}
+                aria-expanded={isOpen} aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className={`collapse navbar-collapse ${isOpen ? 'show' : ''}`}>
+          <div className="navbar-nav me-auto mb-2 mb-lg-0">
+            {nav.map((r, i) => {
+              if (!r.isPrivate && r.isMenu) {
+                return <MenuItem key={i} r={r} onClick={close} />;
+              } else if (user.isAuthenticated && r.isMenu) {
+                return <MenuItem key={i} r={r} onClick={close} />;
+              } else return false;
+            })}
+          </div>
+          <div className="navbar-nav">
+            {user.isAuthenticated ? (
+              <Link className="nav-link" to={"#"} onClick={() => { logout(); close(); }}>
+                Log out
+              </Link>
+            ) : (
+              <Link className="nav-link" to={"/login"} onClick={close}>Log in</Link>
+            )}
+            {!user.isAuthenticated && (
+              <Link className="nav-link"
+                    to={"/signup"}
+                    onClick={(e) => {
+                      e.preventDefault();
+                      navigate("/signup");
+                      close();
+                    }}
+              >
+                Signup
+              </Link>
+            )}
+          </div>
         </div>
-      ) : (
-        <div className="menuItem">
-          <Link to={"/login"}>Log in</Link>
-        </div>
-      )}
-
-      {!user.isAuthenticated && (
-        <div className="menuItem">
-          <Link
-            to={"/signup"}
-            onClick={(e) => {
-              e.preventDefault();
-              navigate("/signup");
-            }}
-          >
-            Signup
-          </Link>
-        </div>
-      )}
-    </div>
+      </div>
+    </nav>
   );
 };
