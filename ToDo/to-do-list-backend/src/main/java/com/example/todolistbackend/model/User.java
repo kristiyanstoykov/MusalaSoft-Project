3e4b7104482a0121de42cd627eee29bd4a39e2a1
Stoian02 <81009844+Stoian02@users.noreply.github.com>
2023-07-21 10:16:54 +0300

    Added tests for update user

--- ToDo/to-do-list-backend/src/main/java/com/example/todolistbackend/model/User.java
+++ ToDo/to-do-list-backend/src/main/java/com/example/todolistbackend/model/User.java
@@ -2,6 +2,7 @@ package com.example.todolistbackend.model;
 
 import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -14,6 +15,7 @@ import java.util.List;
 @NoArgsConstructor
 @Getter
 @Setter
+@EqualsAndHashCode
 @Table(name = "users")
 public class User {
 
